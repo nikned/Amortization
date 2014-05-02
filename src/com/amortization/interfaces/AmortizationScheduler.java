d5f62373f5b1100e8b35b0de8ac1111a1130e060
nik ned <1nikhilnedunuri1@gmail.com>
2014-05-02 00:16:50 -0500

    refactor, updated method signatures, methods now throw exceptions out on error

--- src/com/amortization/interfaces/AmortizationScheduler.java
+++ src/com/amortization/interfaces/AmortizationScheduler.java
@@ -11,11 +11,11 @@ public interface AmortizationScheduler {
 
     public long calculateMonthlyPayment(AmortizationSchedule input, double monthlyInterestDivisor);
 
-    public void outputAmortizationSchedule(AmortizationSchedule input);
+    public void outputAmortizationSchedule(AmortizationSchedule input) throws Exception;
 
     public double calculateMonthlyInterest(double apr, double monthlyInterestDivisor);
 
-    public void generateAmortizationScheduleTable(AmortizationSchedule input, PrintFormatHelper fsHelper);
+    public void generateAmortizationScheduleTable(AmortizationSchedule input, PrintFormatHelper printFormatHelper) throws Exception;
 
     public long calculateCurrMonthPrincipalPaid(long curMonthlyInterest, long curMonthlyPaymentAmount);
 
